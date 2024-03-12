08b07bebf59768b22c9417a069cf056f5a2dc9fb
KozyrevaSvetlana <lanako0507@gmail.com>
2024-03-12 21:45:34 +0300

    тестовый коммит

--- Appointment.Service/Program.cs
+++ Appointment.Service/Program.cs
@@ -10,7 +10,6 @@ using BusinessLogic.Services.Mapping;
 using MassTransit;
 using System.Transactions;
 
-//просто тест для гита
 var builder = WebApplication.CreateBuilder(args);
 IConfiguration configuration = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
